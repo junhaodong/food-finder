54fed941c6197890b07034e1fd1678ffc6c1ea93
fawnwong <fawn.wong@live.com>
2014-11-09 22:55:32 -0500

    it's something

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, url_for, redirect, render_template
+import json
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+	return "this is the index page"
+
+@app.route("/tagged/<tag>")
+def tag(tag="foodporn"):
+	url = "http://api.tumblr.com/v2/tagged?tag=%s&api_key=R3IuczppduJh8tyKOeIISQnDdAR4jRl6CN5ascDxsX1kTpUBq9"
+	url = url%(tag)
+	request = urllib.urlopen(url)
+	s = request.read()
+	result = json.loads(s)
+	retstring = ""
+	for s in result["response"]:
+		print(s)
+		try: 
+			retstring = retstring + "<img src=%s>"%(s['photos'][0]['original_size']['url'])
+			print(retstring)
+		except:
+			pass
+	return retstring
+
+
+if __name__=="__main__":
+	app.debug=True
+	app.run(host="0.0.0.0")
