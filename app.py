eced8140e53f7e8536c01989fdc0991be0a99131
Junhao Dong <junhao.dong96@gmail.com>
2014-11-11 22:24:22 -0500

    added tumblrImages to index page

--- app.py
+++ app.py
@@ -3,28 +3,42 @@ import json, urllib
 
 app=Flask(__name__)
 
+tumblrURL = "http://api.tumblr.com/v2/tagged?tag=%s&api_key=R3IuczppduJ\
+h8tyKOeIISQnDdAR4jRl6CN5ascDxsX1kTpUBq9"
+
+igURL = "https://api.instagram.com/v1/tags/search?q=snowy&access_token=\
+ACCESS-TOKEN"
+
+
+##TO DO
+# need to resize images to scale
+# figure out ig api (if possible)
+##
+
+#return list of image URLs
+def getImageURLs(apiLink, tag):
+        url = apiLink % (tag)
+        data = urllib.urlopen(url).read()
+        result = json.loads(data)
+        list = []
+        for s in result['response']:
+                try:
+                        list.append(s['photos'][0]['alt_sizes'][1]['url'])
+                except:
+                        pass
+        return list
+                
 @app.route("/")
 def index():
-	return render_template("index.html")
-
+        return render_template("index.html",tumblrImages=getImageURLs(tumblrURL,"food"))
+        
 @app.route("/tagged/<tag>")
 def tag(tag="foodporn"):
-	url = "http://api.tumblr.com/v2/tagged?tag=%s&api_key=R3IuczppduJh8tyKOeIISQnDdAR4jRl6CN5ascDxsX1kTpUBq9"
-	url = url%(tag)
-	request = urllib.urlopen(url)
-	s = request.read()
-	result = json.loads(s)
-	retstring = ""
-	for s in result["response"]:
-		print(s)
-		try: 
-			retstring = retstring + "<img src=%s>"%(s['photos'][0]['original_size']['url'])
-			print(retstring)
-		except:
-			pass
-	return retstring
-
-
+        tumblrStr = ""
+	for s in getImageURLs(tumblrURL,tag):
+                tumblrStr+= "<img src=%s>" % s
+        return tumblrStr
+        
 if __name__=="__main__":
-	app.debug=True
+        app.debug=True
 	app.run(host="0.0.0.0")
